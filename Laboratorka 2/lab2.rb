ca5ab51c3cd9f901d64e886d37594f58dbc5a81e
Vitaliy-Zhaldakov <78874453+Vitaliy-Zhaldakov@users.noreply.github.com>
2022-03-13 00:56:21 +0300

    Block 1 Task 2

--- Laboratorka 2/lab2.rb
+++ Laboratorka 2/lab2.rb
@@ -5,4 +5,17 @@ class Department
     @name = name
     @phone = phone
   end
+
+  def getInfo
+    puts "#{@name} -> телефон: #{@phone}"
+  end
 end
+
+hr = Department.new("Отдел кадров", "261-55-11")
+logistics = Department.new("Отдел логистики", "261-46-89")
+finance = Department.new("Отдел финансов", "261-99-66")
+
+puts "----- Информация об отделах -----"
+hr.getInfo
+logistics.getInfo
+finance.getInfo
