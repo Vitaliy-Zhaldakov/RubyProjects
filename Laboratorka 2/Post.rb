ce281e0489005add0fb115539c7247048e756915
Vitaliy-Zhaldakov <78874453+Vitaliy-Zhaldakov@users.noreply.github.com>
2022-04-26 22:20:21 +0300

    Update

--- Laboratorka 2/Post.rb
+++ Laboratorka 2/Post.rb
@@ -1,19 +1,29 @@
-class Post
-  attr_accessor :title, :department, :salary, :vacant
+current_path = File.dirname(__FILE__)
+require "#{current_path}/SalaryDecorator.rb"
 
-  def initialize(title, department, salary, vacant)
-    @title = title
-    @department = department
-    @salary = salary
-    @vacant = vacant
+class Post
+  attr_accessor :title, :department, :vacant
+  attr_writer :salary
+
+  def salary
+    @salary.get_salary
+  end
+
+  def initialize(title:, department:, salary:, percent:nil, rub:nil,
+    premium:nil, fine:nil, vacant:)
+    self.title = title
+    self.department = department
+    self.salary = Salary.get_instance(salary:salary, bonus_percent:percent,
+    bonus_rub: rub, premium: premium, fine: fine)
+    self.vacant = vacant
   end
 
   def to_s
-    if vacant == 1
+    if @vacant == 1
       vacant = "Свободна"
     else
       vacant = "Занята"
     end
-    "#{@title} #{@department} #{@salary}₽ #{vacant}"
+    "#{@title} #{@department} #{@salary.get_salary}₽ #{vacant}"
   end
 end
